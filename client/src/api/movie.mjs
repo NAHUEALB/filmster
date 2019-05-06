8d19737018085a61f4af1589bda71b833232d1ae
juanpablomoscoloni <juampi9611@gmail.com>
2019-05-06 11:12:31 -0300

    Resuelto lo del año.

--- client/src/api/movie.mjs
+++ client/src/api/movie.mjs
@@ -4,9 +4,10 @@ function getAll() {
 }
 
 function create(movie) {
+  const anio=movie.year.getFullYear()
 fetch('/api/v1/movies', {
         method: 'post',
-        body:    JSON.stringify({title:movie.name,description:movie.plot,year:movie.year,runtime:movie.runtime,country:movie.country,language:movie.language,genres:movie.generes,writers:movie.writers,directors:movie.directors}),
+        body:    JSON.stringify({title:movie.name,description:movie.plot,year:anio,runtime:movie.runtime,country:movie.country,language:movie.language,genres:movie.generes,writers:movie.writers,directors:movie.directors}),
         headers: { 	'Accept': 'application/json',
         			'Content-Type': 'application/json' },
     })
