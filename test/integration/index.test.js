421aa9046e37ad0b9910ca19ccf2cff8674dbf8b
juanpablomoscoloni <juampi9611@gmail.com>
2019-05-20 19:57:48 -0300

    Fixeado test 2

--- test/integration/index.test.js
+++ test/integration/index.test.js
@@ -21,9 +21,13 @@ test('Se debería iniciar la aplicación sin películas', async () => {
     expect(movies.length).toBe(0);
 });
 
+
 test('Se debería reflejar en la base de datos la pelicula agregada', async () =>
 {
-	 const movie = {
+
+  const URL = `${baseURL}/movies/`;
+
+	 const movieData = {
         title: 'Back to the Future',
         description: 'Marty McFly, a 17-year-old high school student, is accidentally sent thirty years into the past in a time-traveling DeLorean invented by his close friend, the maverick scientist Doc Brown.',
         year: 1985,
@@ -34,17 +38,23 @@ test('Se debería reflejar en la base de datos la pelicula agregada', async () =
         directors: ['Robert Zemeckis'],
         writers: ['Robert Zemeckis', 'Bob Gale']
     };
-    const movieCreated= await MovieModels.create(movie);
-    const result = await MovieModels.getAll();
+     const movie = await fetch(URL, {
+          method: 'post',
+          body:    JSON.stringify({title:movieData.name,description:movieData.plot,year:movieData.year,
+               runtime:movieData.runtime,country:movieData.country,language:movieData.language,
+               genres:movieData.generes,writers:movieData.writers,directors:movieData.directors})
+    })  ;
+    const movies = await fetch(URL);
+    const result = movies.json();
 
-  expect(result[0].title).toBe(movie.title);
-  expect(result[0].description).toBe(movie.description);
-  expect(result[0].year).toBe(movie.year);
-  expect(result[0].runtime).toBe(movie.runtime);
-  expect(result[0].country).toBe(movie.country);
-  expect(result[0].language).toBe(movie.language);
-  expect(result[0].genres).toEqual(movie.genres);
-  expect(result[0].writers).toEqual(movie.writers);
-  expect(result[0].directors).toEqual(movie.directors);
+  expect(result.title).toBe(movie.title);
+  expect(result.description).toBe(movie.description);
+  expect(result.year).toBe(movie.year);
+  expect(result.runtime).toBe(movie.runtime);
+  expect(result.country).toBe(movie.country);
+  expect(result.language).toBe(movie.language);
+  expect(result.genres).toBe(movie.genres);
+  expect(result.writers).toBe(movie.writers);
+  expect(result.directors).toBe(movie.directors);
 
 });
